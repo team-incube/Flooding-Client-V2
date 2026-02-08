f39843ac4da742f7811ba23f5958c514a149cb6e
moonkanghyeon <s25024@gsm.hs.kr>
2026-02-09 00:46:40 +0900

    feat: userRemove svg 코드 추가

---
+++ src/shared/asset/svg/UserRemove.tsx
@@ -0,0 +1,28 @@
+export default function UserRemove() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.25 7.125C16.25 9.40317 14.4032 11.25 12.125 11.25C9.84683 11.25 8 9.40317 8 7.125C8 4.84683 9.84683 3 12.125 3C14.4032 3 16.25 4.84683 16.25 7.125Z"
+        stroke="#CACACA"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M19.25 18C19.25 20.0711 16.06 21 12.125 21C8.18997 21 5 20.0711 5 18C5 15.9289 8.18997 14.25 12.125 14.25C16.06 14.25 19.25 15.9289 19.25 18Z"
+        stroke="#CACACA"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M18 3H22"
+        stroke="#CACACA"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
