ed81b94a4bf7fc839c1346bf82113c69aae236bf
jyuuuuu0 <s25023@gsm.hs.kr>
2026-02-24 23:58:28 +0900

    fix: SVG 속성명을 JSX 규칙에 맞게 수정

--- src/shared/asset/svg/Cancel.tsx
+++ src/shared/asset/svg/Cancel.tsx
@@ -21,9 +21,9 @@ export default function Cancel() {
           width="32"
           height="32"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
