cd69eb75eac33e45e96e0d920a67999ebbbce6e3
jyuuuuu0 <s25023@gsm.hs.kr>
2026-02-25 22:16:15 +0900

    feat: Profile svg 생성

---
+++ src/shared/asset/svg/Profile.tsx
@@ -0,0 +1,31 @@
+export default function Profile() {
+  return (
+    <svg
+      width="82"
+      height="82"
+      viewBox="0 0 82 82"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_498_666)">
+        <rect width="82" height="82" rx="41" fill="var(--color-sub-3)" />
+        <ellipse cx="41" cy="71" rx="29" ry="17" fill="var(--background)" />
+        <circle cx="41" cy="36" r="14" fill="var(--background)" />
+      </g>
+      <rect
+        x="2"
+        y="2"
+        width="78"
+        height="78"
+        rx="39"
+        stroke="var(--color-sub-3)"
+        strokeWidth="4"
+      />
+      <defs>
+        <clipPath id="clip0_498_666">
+          <rect width="82" height="82" rx="41" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
