e9b198f5aaf40c894dda3a719ceb6fd3e9ee32c0
jyuuuuu0 <s25023@gsm.hs.kr>
2026-02-25 20:47:58 +0900

    feat: Sun svg 생성

---
+++ src/shared/asset/svg/Sun.tsx
@@ -0,0 +1,77 @@
+export default function Sun() {
+  return (
+    <svg
+      width="31"
+      height="31"
+      viewBox="0 0 31 31"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="15.5" cy="15.5" r="15.5" fill="white" />
+      <circle
+        cx="15.3702"
+        cy="15.3699"
+        r="4.8825"
+        fill="var(--color-sub-2)"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.395"
+      />
+      <path
+        d="M15.3704 7V7.36981"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.3704 23.3698V23.7399"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7 15.37L7.36981 15.37"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.3698 15.37L23.74 15.37"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.4519 9.45105L9.7134 9.71255"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21.0271 21.0262L21.2889 21.288"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.45148 21.2883L9.71297 21.0268"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21.0266 9.71326L21.2883 9.4515"
+        stroke="var(--color-sub-2)"
+        stroke-width="1.86"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
