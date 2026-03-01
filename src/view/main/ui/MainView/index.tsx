c0f0090d2d6e378eabfe3b2f930524a18e155787
koreahghg <koreahghg@gmail.com>
2026-03-01 10:43:25 +0900

    feat: 컴포넌트 적용

--- src/view/main/ui/MainView/index.tsx
+++ src/view/main/ui/MainView/index.tsx
@@ -1,11 +1,14 @@
 import Sidebar from "@/widgets/adaptive-sidebar/ui";
 import { DarkModeToggle } from "@/shared/ui/Toggle/DarkModeToggle";
+import ApplyCard from "@/widgets/main/ui/ApplyCard";
+import ChairIcon from "@/shared/asset/svg/Chair";
+import BookIcon from "@/shared/asset/svg/ApplyStudy";
 
 export default function MainView() {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 overflow-auto px-18 pt-13 pb-25">
+      <main className="flex-1 overflow-auto px-18 pt-13 pb-25">
         <div className="flex items-end justify-between">
           <div className="flex items-end gap-3">
             <span className="text-size-title-2 font-bold text-main-text leading-none">
@@ -24,7 +27,27 @@ export default function MainView() {
 
           <DarkModeToggle />
         </div>
-      </div>
+
+        <ApplyCard
+          title="자습신청"
+          icon={<BookIcon />}
+          current={4}
+          total={50}
+          timeText="자습 신청 시간은 20:00 ~ 21:00에 신청이 가능해요"
+          buttonText="신청 불가"
+          disabled
+        />
+
+        <ApplyCard
+          title="안마의자"
+          icon={<ChairIcon />}
+          current={4}
+          total={5}
+          timeText="안마 의자 신청 시간은 20:20 ~ 21:00에 신청이 가능해요"
+          buttonText="신청"
+          femaleNotice
+        />
+      </main>
     </div>
   );
 }
