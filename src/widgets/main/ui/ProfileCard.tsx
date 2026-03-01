7a2dd9abb3f06e1c46f3c0e5b4ff31bb5600188c
koreahghg <koreahghg@gmail.com>
2026-03-01 17:35:56 +0900

    feat: 프로필 컴포넌트 수정

--- src/widgets/main/ui/ProfileCard.tsx
+++ src/widgets/main/ui/ProfileCard.tsx
@@ -3,16 +3,10 @@ import ProfileSvg from "@/shared/asset/svg/Profile";
 import More from "@/shared/asset/svg/MoreVertical";
 
 interface ProfileCardProps {
-  name: string;
-  studentId?: string;
   photo?: ReactNode;
 }
 
-export default function ProfileCard({
-  name,
-  studentId,
-  photo,
-}: ProfileCardProps) {
+export default function ProfileCard({ photo }: ProfileCardProps) {
   return (
     <div className="flex items-center gap-6 p-6 bg-background-surface rounded-2xl w-[480px]">
       <div className="w-[72px] h-[72px] flex items-center justify-center">
@@ -20,13 +14,9 @@ export default function ProfileCard({
       </div>
       <div className="flex flex-col">
         <span className="font-medium text-size-title-2">
-          안녕하세요! {name}님
+          안녕하세요! 민솔님
         </span>
-        {studentId && (
-          <span className="text-size-text-3 font-medium text-sub-1">
-            {studentId}
-          </span>
-        )}
+        <span className="text-size-text-3 font-medium text-sub-1">2403</span>
       </div>
       <div className="ml-auto">
         <More />
