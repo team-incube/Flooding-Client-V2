acb09372beeb1769615d445ce1bb9f03dafa58c9
koreahghg <koreahghg@gmail.com>
2026-03-01 18:57:08 +0900

    feat: 기상음악 신청 컴포넌트 적용

--- src/view/main/ui/MainView/index.tsx
+++ src/view/main/ui/MainView/index.tsx
@@ -6,6 +6,7 @@ import BookIcon from "@/shared/asset/svg/ApplyStudy";
 import Header from "@/widgets/main/ui/Header";
 import ProfileCard from "@/widgets/main/ui/ProfileCard";
 import TimeTableCard from "@/widgets/main/ui/TimeTableCard";
+import MusicRequestCard from "@/widgets/main/ui/MusicRequestCard";
 
 export default function MainView() {
   return (
@@ -13,7 +14,7 @@ export default function MainView() {
       <Sidebar />
       <main className="flex-1 overflow-auto px-18 pt-13 pb-25">
         <Header />
-        <div className="flex gap-6">
+        <div className="flex gap-6 mb-6">
           <div className="flex flex-col gap-6">
             <ApplyCard
               title="자습신청"
@@ -41,6 +42,7 @@ export default function MainView() {
             <TimeTableCard />
           </div>
         </div>
+        <MusicRequestCard />
       </main>
     </div>
   );
