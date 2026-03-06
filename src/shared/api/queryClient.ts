6239fc7efc0963b7ba44069fbf0fd6adc43abd12
ma94275 <s25020@gsm.hs.kr>
2026-03-06 17:56:51 +0900

    feat: QueryClient 전역 옵션 설정

---
+++ src/shared/api/queryClient.ts
@@ -0,0 +1,13 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 30,
+      gcTime: 1000 * 60 * 60,
+      retry: 1,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+    }
+  }
+});
