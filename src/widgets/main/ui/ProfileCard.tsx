26b9b4afd659eebc58a128139457bc2fdf0fac3a
koreahghg <koreahghg@gmail.com>
2026-03-07 18:58:05 +0900

    style: ProfileCard 레이아웃

--- src/widgets/main/ui/ProfileCard.tsx
+++ src/widgets/main/ui/ProfileCard.tsx
@@ -10,13 +10,13 @@ interface ProfileCardProps {
 
 export default function ProfileCard({ photo, user }: ProfileCardProps) {
   return (
-    <div className="flex items-center gap-6 p-4 2xl:p-6 bg-background-surface rounded-2xl w-full">
-      <div className="w-[72px] h-[72px] flex items-center justify-center">
+    <div className="flex items-center gap-6 p-6 bg-background-surface rounded-2xl w-full h-35 2xl:w-120 2xl:h-30">
+      <div className="w-18 flex items-center justify-center">
         {photo ?? <ProfileSvg />}
       </div>
       <div className="flex flex-col">
         <span className="font-medium text-title-4">
-          안녕하세요! {user?.name ?? ""}님
+          {user?.name ?? ""}
         </span>
         <span className="text-text-3 font-medium text-sub-1">
           {user?.studentNumber ?? ""}
