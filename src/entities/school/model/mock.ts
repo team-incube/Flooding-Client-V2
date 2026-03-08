f4b304b96c3aed11f9a40a36c3317c3bc45dd424
koreahghg <koreahghg@gmail.com>
2026-03-09 08:17:49 +0900

    feat: 학교 홈베이스 예약 목 데이터 추가

---
+++ src/entities/school/model/mock.ts
@@ -0,0 +1,52 @@
+export const MOCK_MY_RESERVATION = {
+  tableName: "테이블 3",
+  floor: "2F",
+  members: ["2501 박건우"],
+  periods: ["8교시", "9교시"],
+  reason: "팀 프로젝트",
+};
+
+export const MOCK_RESERVATIONS = [
+  {
+    tableName: "테이블 1",
+    floor: "2F",
+    members: ["2501 박건우", "2502 이수진"],
+    periods: ["1교시", "2교시"],
+    reason: "팀 프로젝트",
+  },
+  {
+    tableName: "테이블 2",
+    floor: "3F",
+    members: ["1301 김태양"],
+    periods: ["3교시"],
+    reason: "자습",
+  },
+  {
+    tableName: "테이블 3",
+    floor: "4F",
+    members: ["1302 최아름", "3401 정민호", "2201 한지원"],
+    periods: ["2교시", "3교시", "4교시"],
+    reason: "스터디",
+  },
+  {
+    tableName: "테이블 4",
+    floor: "2F",
+    members: ["1101 오준혁", "2302 윤서연", "3201 강동현", "1402 신예린"],
+    periods: ["1교시"],
+    reason: "조별 과제",
+  },
+  {
+    tableName: "테이블 5",
+    floor: "3F",
+    members: ["3301 임재원", "2101 배수아"],
+    periods: ["4교시", "5교시"],
+    reason: "시험 준비",
+  },
+  {
+    tableName: "테이블 6",
+    floor: "4F",
+    members: ["1201 조현준", "3101 권나연", "2401 문성민", "1501 류하은", "3501 서지훈"],
+    periods: ["2교시"],
+    reason: "프로젝트 발표 준비",
+  },
+];
