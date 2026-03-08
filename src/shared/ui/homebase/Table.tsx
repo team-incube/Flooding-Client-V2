eec6d94cab316e27330fdac3327fbe5c11730f28
leehyoeun <s25049@gsm.hs.kr>
2026-03-08 13:27:07 +0900

    feat: 층별 테이블 선택 상태에 따른 배경 스타일 추가

--- src/shared/ui/homebase/Table.tsx
+++ src/shared/ui/homebase/Table.tsx
@@ -3,13 +3,16 @@ interface TableProps {
   capacity: string;
   className?: string;
   onClick?: () => void;
+  selected?: boolean;
 }
 
-export function Table({ name, capacity, className = "", onClick }: TableProps) {
+export function Table({ name, capacity, className = "", onClick, selected }: TableProps) {
   return (
     <button
       onClick={onClick}
-      className={`flex flex-col items-center justify-center gap-1 cursor-pointer ${className}`}
+      className={`flex flex-col items-center justify-center gap-1 cursor-pointer
+      ${selected ? "bg-p-2" : "bg-background-surface"}
+      ${className}`}
     >
       <div className="text-sub-2 text-[18.93px] font-medium">{name}</div>
       <div className="text-p-1 text-[16.82px] font-medium">{capacity}</div>
