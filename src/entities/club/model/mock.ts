5a70f92b3ac4af8d9d07c49b9a41b59bb3e26eb6
moonkanghyeon <s25024@gsm.hs.kr>
2026-03-12 00:43:53 +0900

    feat: 동아리 mock 데이터 생성

---
+++ src/entities/club/model/mock.ts
@@ -0,0 +1,104 @@
+import { Club } from "./club";
+
+export const MOCK_CLUBS: Club[] = [
+  {
+    id: 1,
+    name: "인력사무소",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "박건우",
+    memberCount: 12,
+  },
+  {
+    id: 2,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "이수진",
+    memberCount: 8,
+  },
+  {
+    id: 3,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "김태양",
+    memberCount: 15,
+  },
+  {
+    id: 4,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "최아름",
+    memberCount: 10,
+  },
+  {
+    id: 5,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "정민호",
+    memberCount: 7,
+  },
+  {
+    id: 6,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "한지원",
+    memberCount: 9,
+  },
+  {
+    id: 7,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "오준혁",
+    memberCount: 11,
+  },
+  {
+    id: 8,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "윤서연",
+    memberCount: 6,
+  },
+  {
+    id: 9,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "강동현",
+    memberCount: 13,
+  },
+  {
+    id: 10,
+    name: "동아리명",
+    description:
+      "동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명동아리 설명",
+    tags: ["세탁기", "디자이너_급구"],
+    thumbnailUrl: "/hq720.jpg",
+    presidentName: "신예린",
+    memberCount: 5,
+  },
+];
