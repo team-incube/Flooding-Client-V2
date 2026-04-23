ee57866aeffccfc0691f5b9ce894a90f760b18bc
Dino0204 <s24020@gsm.hs.kr>
2026-04-23 10:21:04 +0900

    chore: i.ytimg.com 이미지 도메인 허용 추가

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.youtube.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
     ],
   },
   turbopack: {
