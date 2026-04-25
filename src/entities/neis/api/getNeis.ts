bf4446b3ea6b2d75024d737ea880adc4d1e3cb27
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-26 04:27:14 +0900

    feat: neis 급식, 시간표 API 함수 생성

---
+++ src/entities/neis/api/getNeis.ts
@@ -0,0 +1,48 @@
+import { instance } from "@/shared/api/instance";
+import type {
+  MealItem,
+  Period,
+  TimetableParams,
+} from "@/entities/neis/model/neis";
+
+interface MealsResponse {
+  data: {
+    date: string;
+    meals: MealItem[];
+  };
+}
+
+interface TimetableResponse {
+  data: {
+    date: string;
+    grade: number;
+    classNumber: number;
+    periods: Period[];
+  };
+}
+
+const apiKey = process.env.NEXT_PUBLIC_NEIS_API_KEY;
+
+export async function getTimetables(
+  params: TimetableParams,
+): Promise<Period[]> {
+  const { data } = await instance.get<TimetableResponse>(
+    "/v2/neis/timetables",
+    {
+      params,
+    },
+  );
+  return data.data.periods;
+}
+
+export async function getMeals(date: string): Promise<MealItem[]> {
+  const { data } = await instance.get<MealsResponse>(
+    `/v2/neis/meals?&date=${date}`,
+    {
+      headers: {
+        "X-API-KEY": apiKey,
+      },
+    },
+  );
+  return data.data.meals;
+}
