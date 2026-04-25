c103a62ae053fa46f62c8e90ee332327b788f815
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-26 04:31:34 +0900

    feat: 사용자 정보 API 연동

--- src/widgets/main/ui/ProfileCard.tsx
+++ src/widgets/main/ui/ProfileCard.tsx
@@ -1,18 +1,17 @@
-import { ReactNode } from "react";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 import ProfileSvg from "@/shared/asset/svg/Profile";
 import More from "@/shared/asset/svg/MoreVertical";
-import { User } from "@/entities/user/model/user";
+import { userQueries } from "@/entities/user/api/userQueries";
 
-interface ProfileCardProps {
-  photo?: ReactNode;
-  user?: User;
-}
+export default function ProfileCard() {
+  const { data: user } = useQuery(userQueries.me());
 
-export default function ProfileCard({ photo, user }: ProfileCardProps) {
   return (
     <div className="flex items-center gap-6 p-6 bg-background-surface rounded-2xl w-full h-35 2xl:w-120 2xl:h-30">
       <div className="w-18 flex items-center justify-center">
-        {photo ?? <ProfileSvg />}
+        <ProfileSvg />
       </div>
       <div className="flex flex-col">
         <span className="font-medium text-title-4">{user?.name ?? ""}</span>
