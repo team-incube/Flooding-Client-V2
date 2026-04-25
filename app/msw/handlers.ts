da0c4f5c3e372ecaed975c09ff60a3248aac6384
koreahghg <koreahghg@gmail.com>
2026-04-25 16:48:43 +0900

    remove: homebase MSW 목 핸들러 제거

--- app/msw/handlers.ts
+++ app/msw/handlers.ts
@@ -1,4 +1,3 @@
 import { dormitoryHandlers } from "@/entities/dormitory/api/dormitoryHandlers";
-import { homebaseHandlers } from "@/entities/homebase/api/homebaseHandlers";
 
-export const handlers = [...dormitoryHandlers, ...homebaseHandlers];
+export const handlers = [...dormitoryHandlers];
