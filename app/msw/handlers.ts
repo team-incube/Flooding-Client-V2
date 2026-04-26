d66411a7019dace9b3b77692eeca2bbe7f54f692
g-hyxn <s25024@gsm.hs.kr>
2026-04-27 01:34:55 +0900

    fix: handlers.ts에서 삭제된 homebaseHandlers import 제거

--- app/msw/handlers.ts
+++ app/msw/handlers.ts
@@ -1,5 +1,4 @@
 import { dormitoryHandlers } from "@/entities/dormitory/api/dormitoryHandlers";
-import { homebaseHandlers } from "@/entities/homebase/api/homebaseHandlers";
 import { clubHandlers } from "@/entities/club/api/clubHandlers";
 
-export const handlers = [...dormitoryHandlers, ...homebaseHandlers, ...clubHandlers];
+export const handlers = [...dormitoryHandlers, ...clubHandlers];
