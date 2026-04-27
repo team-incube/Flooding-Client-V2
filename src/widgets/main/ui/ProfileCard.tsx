cc24c31a5306cfb7586763d03976276ad4e90045
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-27 18:18:50 +0900

    feat: 2xl 이상 화면에서 인사말 반응형 처리

--- src/widgets/main/ui/ProfileCard.tsx
+++ src/widgets/main/ui/ProfileCard.tsx
@@ -9,12 +9,17 @@ export default function ProfileCard() {
   const { data: user } = useQuery(userQueries.me());
 
   return (
-    <div className="flex items-center gap-6 p-6 bg-background-surface rounded-2xl w-full h-35 2xl:w-120 2xl:h-30">
+    <div className="flex items-center gap-6 p-6 bg-background-surface rounded-2xl w-full h-35 2xl:h-30">
       <div className="w-18 flex items-center justify-center">
         <ProfileSvg />
       </div>
       <div className="flex flex-col">
-        <span className="font-medium text-title-4">{user?.name ?? ""}</span>
+        <span className="font-medium text-title-4">
+          <span className="hidden 2xl:inline">
+            안녕하세요! {user ? `${user.name.slice(1)}님` : ""}
+          </span>
+          <span className="2xl:hidden">{user?.name ?? ""}</span>
+        </span>
         <span className="text-text-3 font-medium text-sub-1">
           {user?.studentNumber ?? ""}
         </span>
