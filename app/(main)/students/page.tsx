6fd87bd0f5496922c06e827bda462cee69e34fe5
Dino0204 <s24020@gsm.hs.kr>
2026-04-28 13:09:46 +0900

    change: 학생·기숙사 페이지 패딩 정리

--- app/(main)/students/page.tsx
+++ app/(main)/students/page.tsx
@@ -2,7 +2,7 @@ import { StudentManagementSection } from "@/features/self-study/ui/StudentManage
 
 export default function StudentsPage() {
   return (
-    <main className="flex flex-1 flex-col overflow-y-auto p-6">
+    <main className="flex flex-1 flex-col overflow-y-auto px-8 lg:px-10 2xl:px-18 pb-25">
       <StudentManagementSection />
     </main>
   );
