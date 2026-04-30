1307b308c2f5bbb5eae428f700ca443d88e369b2
Dino0204 <s24020@gsm.hs.kr>
2026-04-30 10:41:16 +0900

    change: 사용자 타입의 의도를 명확하게 전달하도록 변경

--- src/entities/user/ui/ProfileCard.tsx
+++ src/entities/user/ui/ProfileCard.tsx
@@ -2,10 +2,7 @@ import type { User } from "@/entities/user/model/user";
 import Gender from "@/shared/asset/svg/Gender";
 import Profile from "@/shared/asset/svg/Profile";
 
-type ProfileCardStudent = Omit<
-  User,
-  Exclude<keyof User, "name" | "studentNumber" | "sex">
->;
+type ProfileCardStudent = Pick<User, "name" | "studentNumber" | "sex">;
 
 interface ProfileCardProps {
   index: number;
