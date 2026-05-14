c206e336081e23cd78405d849887929e9dddbd61
Dino0204 <s24020@gsm.hs.kr>
2026-05-14 13:36:27 +0900

    chore: 자습, 안마의자 신청 종료 시간 분리

--- src/entities/dormitory/lib/applicationTime.ts
+++ src/entities/dormitory/lib/applicationTime.ts
@@ -1,12 +1,10 @@
 import { isTimeInWindow } from "@/shared/lib/timeWindow";
 
-const APPLICATION_END_TIME = { hour: 21, minute: 0 };
-
 export function isStudyApplicationTime(date: Date): boolean {
   return isTimeInWindow({
     date,
     start: { hour: 20, minute: 0 },
-    end: APPLICATION_END_TIME,
+    end: { hour: 21, minute: 0 },
   });
 }
 
@@ -14,6 +12,6 @@ export function isMassageApplicationTime(date: Date): boolean {
   return isTimeInWindow({
     date,
     start: { hour: 20, minute: 20 },
-    end: APPLICATION_END_TIME,
+    end: { hour: 21, minute: 0 },
   });
 }
